9b42d423814e85b11625d953bf916e7e05bfa57a
Ryan Woo <devrwoo@gmail.com>
2019-01-07 17:49:27 -0800

    Aciton for createPost

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST
+} from './types';
 
 //GET POSTS
 export const getPosts = () => async dispatch => {
@@ -60,6 +66,29 @@ export const deletePost = postId => async dispatch => {
     });
 
     dispatch(setAlert('POST REMOVED', 'success'));
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+//ADD POSTS
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  try {
+    const res = await axios.post(`/api/posts/`, formData, config);
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert('POST CREATED', 'success'));
   } catch (error) {
     dispatch({
       type: POST_ERROR,
