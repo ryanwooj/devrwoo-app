c186424a1a72f0b8e76540d12404975865dfc3d1
Ryan Woo <devrwoo@gmail.com>
2019-01-10 18:19:09 -0800

    Posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 
 const Posts = ({ getPosts, posts: { posts, loading } }) => {
   useEffect(() => {
@@ -19,6 +20,7 @@ const Posts = ({ getPosts, posts: { posts, loading } }) => {
         Welcome to the Community
       </p>
       {/* {PostForm} */}
+      <PostForm />
       <div className='posts'>
         {posts.map(post => (
           <PostItem key={post._id} post={post} />
