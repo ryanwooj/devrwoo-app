a1174b73f42c69401cbe1dd2f5faf169deb41814
Ryan Woo <devrwoo@gmail.com>
2019-02-04 18:00:53 -0800

    Auth compoment Login and Register complete

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,15 +1,39 @@
-import React, { Fragment, useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import React, { useState } from 'react';
+// import FacebookLogin from 'react-facebook-login';
+import { Link as RouterLink, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { login } from '../../actions/auth';
+import { login, register, checkUser } from '../../actions/auth';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Icon from '@material-ui/core/Icon';
 
-const Login = ({ login, isAuthenticated }) => {
+const Login = ({
+  login,
+  isAuthenticated,
+  checkUser,
+  loading,
+  status,
+  register,
+  lang: { currentLanguage }
+}) => {
   const [formData, setFormData] = useState({
     email: '',
-    password: ''
+    password: '',
+    avatar: '',
+    name: ''
   });
-
   const { email, password } = formData;
 
   const onChange = e =>
@@ -20,59 +44,150 @@ const Login = ({ login, isAuthenticated }) => {
     login({ email, password });
   };
 
+  const classes = materialStyles();
+
+  const lang = currentLanguage;
+
   //Redirect if logged in
 
   if (isAuthenticated) {
-    return <Redirect to='/dashboard' />;
+    return <Redirect to='/profiles' />;
   }
 
   return (
-    <Fragment>
-      <h1 className='large text-primary'>Sign In</h1>
-      <p className='lead'>
-        <i className='fas fa-user' /> Sign Into Your Account
-      </p>
-      <form className='form' onSubmit={e => onSubmit(e)}>
-        <div className='form-group'>
-          <input
-            type='email'
-            placeholder='Email Address'
-            name='email'
-            value={email}
-            onChange={e => onChange(e)}
-            required
-          />
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Password'
-            name='password'
-            minLength='6'
-            value={password}
-            onChange={e => onChange(e)}
-          />
-        </div>
+    !loading && (
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <Icon>lock_outlined</Icon>
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            {lang.login.title}
+          </Typography>
+          <form className={classes.form} onSubmit={e => onSubmit(e)} noValidate>
+            <TextField
+              variant='outlined'
+              margin='normal'
+              name='email'
+              value={email}
+              onChange={e => onChange(e)}
+              label='Email Address'
+              autoComplete='email'
+              autoFocus
+              fullWidth
+              required
+            />
+            <TextField
+              variant='outlined'
+              margin='normal'
+              minLength='6'
+              name='password'
+              label='Password'
+              type='password'
+              value={password}
+              onChange={e => onChange(e)}
+              autoComplete='current-password'
+              fullWidth
+              required
+            />
+            <FormControlLabel
+              control={<Checkbox value='remember' color='primary' />}
+              label={lang.login.remember}
+            />
 
-        <input type='submit' className='btn btn-primary' value='Login' />
-      </form>
-      <p className='my-1'>
-        Back to <Link to='/'>Home</Link>
-      </p>
-    </Fragment>
+            <Button
+              variant='contained'
+              type='submit'
+              className={classes.submit}
+              color='primary'
+              fullWidth>
+              {lang.login.title}
+            </Button>
+          </form>
+          <Grid item>
+            <Link component={RouterLink} to='/register' variant='body2'>
+              {lang.login.question}
+            </Link>
+          </Grid>
+        </Paper>
+        <Box mt={4} className={classes.boxBottom}>
+          <Typography variant='body2' color='textSecondary' align='center'>
+            {lang.login.back}
+            <Link component={RouterLink} to='/' color='inherit'>
+              {' '}
+              {lang.login.back2}
+            </Link>
+          </Typography>
+        </Box>
+      </Container>
+    )
   );
 };
 
+const materialStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgorundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(15),
+    padding: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  boxBottom: {
+    marginBottom: theme.spacing(17)
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
 Login.propTypes = {
   login: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool
+  isAuthenticated: PropTypes.bool,
+  register: PropTypes.func.isRequired,
+  checkUser: PropTypes.func.isRequired,
+  status: PropTypes.bool
 };
 
 const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
+  isAuthenticated: state.auth.isAuthenticated,
+  status: state.auth.facebook,
+  loading: state.auth.loading,
+  lang: state.language
 });
 
 export default connect(
   mapStateToProps,
-  { login }
+  { login, register, checkUser }
 )(Login);
+
+//       <FacebookLogin
+//         appId='482249872546915'
+//         autoLoad={true}
+//         fields='name,email,picture'
+//         onClick={e => checkUser({ email })}
+//         callback={responseFacebook}
+//       />;
+
+// async function responseFacebook({ name, email, userID, picture }) {
+//   checkUser({ email });
+//   const password = userID;
+//   const avatar = picture.data.url;
+
+//   !loading && Boolean(status)
+//     ? register({ name, email, password, avatar })
+//     : login({ email, password });
+// }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,27 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Link, Redirect } from 'react-router-dom';
+import { Link as RouterLink, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import Icon from '@material-ui/core/Icon';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 // import axios from 'axios';
 
-const Register = ({ setAlert, register, isAuthenticated }) => {
+const Register = ({
+  setAlert,
+  register,
+  isAuthenticated,
+  lang: { currentLanguage, loading }
+}) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -16,6 +30,9 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
   });
 
   const { name, email, password, password2 } = formData;
+  const lang = currentLanguage;
+
+  const classes = useStyles();
 
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -56,74 +73,127 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
   }
 
   return (
-    <Fragment>
-      <h1 className='large text-primary'>Sign Up</h1>
-      <p className='lead'>
-        <i className='fas fa-user' /> Create Your Account
-      </p>
-      <form
-        className='form'
-        action='create-profile.html'
-        onSubmit={e => onSubmit(e)}>
-        <div className='form-group'>
-          <input
-            type='text'
-            placeholder='Name'
-            name='name'
-            value={name}
-            onChange={e => onChange(e)}
-          />
-        </div>
-        <div className='form-group'>
-          <input
-            type='email'
-            placeholder='Email Address'
-            name='email'
-            value={email}
-            onChange={e => onChange(e)}
-          />
-          <small className='form-text'>
-            This site uses Gravatar so if you want Link profile image, use Link
-            Gravatar email
-          </small>
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Password'
-            name='password'
-            minLength='6'
-            value={password}
-            onChange={e => onChange(e)}
-          />
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Confirm Password'
-            name='password2'
-            minLength='6'
-            value={password2}
-            onChange={e => onChange(e)}
-          />
-        </div>
-        <input type='submit' className='btn btn-primary' value='Register' />
-      </form>
-      <p className='my-1'>
-        Already have an account? <Link to='/login'>Sign In</Link>
-      </p>
-    </Fragment>
+    !loading && (
+      <Container component='main' maxWidth='md'>
+        <Paper className={classes.paper}>
+          <Typography variant='h1'>{lang.register.title}</Typography>
+          <Icon>user</Icon>
+          <Typography variant='body1'>{lang.register.subtitle}</Typography>
+          <form
+            className='form'
+            action='create-profile.html'
+            onSubmit={e => onSubmit(e)}>
+            <TextField
+              type='text'
+              varian='outlined'
+              margin='normal'
+              name='name'
+              value={name}
+              label={lang.register.signup}
+              autoComplete='name'
+              autoFocus
+              fullWidth
+              required
+              onChange={e => onChange(e)}
+            />
+            <TextField
+              variant='outlined'
+              margin='normal'
+              name='email'
+              value={email}
+              onChange={e => onChange(e)}
+              label='Email Address'
+              autoComplete='email'
+              autoFocus
+              fullWidth
+            />
+            <Typography variant='body2' align='center'>
+              {lang.register.text}
+            </Typography>
+            <TextField
+              variant='outlined'
+              margin='normal'
+              minLength='6'
+              name='password'
+              label='Password'
+              type='password'
+              value={password}
+              onChange={e => onChange(e)}
+              autoComplete='current-password'
+              fullWidth
+              required
+            />
+            <TextField
+              variant='outlined'
+              margin='normal'
+              minLength='6'
+              name='password2'
+              label='Confirm Password'
+              type='password'
+              value={password2}
+              onChange={e => onChange(e)}
+              autoComplete='current-password'
+              fullWidth
+              required
+            />
+            <Button
+              variant='contained'
+              type='submit'
+              className={classes.submit}
+              color='primary'
+              fullWidth>
+              {' '}
+              {lang.register.regi}
+            </Button>
+          </form>
+          <Grid item>
+            <Link component={RouterLink} to='/register' variant='body2'>
+              {lang.register.back2}
+            </Link>
+          </Grid>
+        </Paper>
+      </Container>
+    )
   );
 };
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgorundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(15),
+    marginBottom: theme.spacing(5),
+    padding: theme.spacing(6),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
   register: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool
+  isAuthenticated: PropTypes.bool,
+  lang: PropTypes.object
 };
 
 const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
+  isAuthenticated: state.auth.isAuthenticated,
+  lang: state.language
 });
 
 export default connect(
