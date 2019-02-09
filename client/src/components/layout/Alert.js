7d60d04504e684ff43ad8f3f72ccb4ed0b83c2a8
Ryan Woo <devrwoo@gmail.com>
2019-02-08 18:01:44 -0800

    layouts for the view added

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,15 +1,69 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import green from '@material-ui/core/colors/green';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Alert = ({ alerts }) =>
-  alerts !== null &&
-  alerts.length > 0 &&
-  alerts.map(alert => (
-    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-      {alert.msg}
-    </div>
-  ));
+const Alert = ({ alerts }) => {
+  const classes = useStyles();
+  return (
+    alerts !== null &&
+    alerts.length > 0 &&
+    alerts.map((alert, index) => {
+      if (alert.alertType === 'success') {
+        return (
+          <Grid container alignItems='center' justify='center' key={index}>
+            <SnackbarContent
+              key={alert.id}
+              className={classes.success}
+              autohideduration={4000}
+              message={alert.msg}
+            />
+          </Grid>
+        );
+      } else if (alert.alertType === 'danger') {
+        return (
+          <Grid container alignItems='center' justify='center' key={index}>
+            <SnackbarContent
+              key={alert.id}
+              className={classes.error}
+              autohideduration={4000}
+              message={alert.msg}
+            />
+          </Grid>
+        );
+      } else {
+        return (
+          <Grid container alignItems='center' justify='center' key={index}>
+            <SnackbarContent
+              key={alert.id}
+              className={classes.error}
+              autohideduration={4000}
+              message={alert.msg}
+            />
+          </Grid>
+        );
+      }
+    })
+  );
+};
+
+const useStyles = makeStyles(theme => ({
+  success: {
+    backgroundColor: green[600],
+    width: '50%',
+    justifyContent: 'center',
+    marginTop: theme.spacing(10)
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+    width: '50%',
+    justifyContent: 'center',
+    marginTop: theme.spacing(10)
+  }
+}));
 
 Alert.propTypes = {
   alerts: PropTypes.array.isRequired
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,41 +1,283 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+import wallpaper from '../../images/wallpaper.jpg';
+import tomasso from '../../images/tomasso.jpg';
+import milada from '../../images/milada.jpg';
 
-const Landing = ({ isAuthenticated }) => {
-  if (isAuthenticated) {
-    return <Redirect to='/dashboard' />;
-  }
-
+const Landing = ({ isAuthenticated, lang: { loading, currentLanguage } }) => {
+  const classes = useStyles();
+  const lang = currentLanguage;
+  // if (isAuthenticated) {
+  //   return <Redirect to='/dashboard' />;
+  // }
   return (
-    <section className='landing'>
-      <div className='dark-overlay'>
-        <div className='landing-inner'>
-          <h1 className='x-large'>Ryan Woo Connector</h1>
-          <p className='lead'>
-            I would like to get to know you. Please Login to Access
-          </p>
-          <div className='buttons'>
-            <Link to='/register' className='btn btn-primary'>
-              Get In Touch
-            </Link>
-            <Link to='/login' className='btn btn-light'>
-              Login
-            </Link>
-          </div>
-        </div>
-      </div>
-    </section>
+    !loading && (
+      <>
+        <Grid
+          container
+          className={classes.media}
+          justify='center'
+          alignItems='center'>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Card>
+              <Typography
+                style={{
+                  position: 'absolute',
+                  top: '300px',
+                  left: '20px',
+                  color: '#ffffff',
+                  fontWeight: 500,
+                  fontSize: '30px'
+                }}>
+                {lang.landing.cover} <br />
+                {lang.landing.cover2}
+              </Typography>
+              <CardMedia
+                className={classes.media}
+                image={wallpaper}
+                title='Dark wallpaper'
+              />
+            </Card>
+          </Grid>
+        </Grid>
+        <Container maxWidth='lg'>
+          <Grid container direction='row' justify='flex-start'>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card className={classes.cardInfo}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography variant='subtitle1'>
+                      {lang.landing.paper1title}
+                    </Typography>
+                    <Typography variant='h5' style={{ marginTop: '10px' }}>
+                      {lang.landing.paper1subtitle}
+                    </Typography>
+                    <Typography variant='body2' style={{ marginTop: '10px' }}>
+                      {lang.landing.paper1text}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size='small' color='primary' to='/' component={Link}>
+                    {lang.landing.paper1button}
+                  </Button>
+                  <Button size='small' color='primary' to='/' component={Link}>
+                    {lang.landing.paper1button2}
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card className={classes.cardInfo}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography variant='subtitle1'>STOCK</Typography>
+                    <Typography
+                      variant='subtitle2'
+                      style={{ marginTop: '20px' }}>
+                      {lang.landing.paper2title}
+                    </Typography>
+                    <Typography variant='h4' style={{ display: 'flex' }}>
+                      $ 1091.01{' '}
+                      <Typography variant='body2' style={{ marginTop: '10px' }}>
+                        <Icon className={classes.iconColor}>show_chart</Icon>
+                        {'   '}
+                        +11.91
+                      </Typography>
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size='small' color='primary' to='/' component={Link}>
+                    {lang.landing.paper2button}
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card className={classes.cardInfo}>
+                <CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant='subtitle1'>
+                      {lang.landing.paper3title}
+                    </Typography>
+                    <Typography
+                      variant='caption'
+                      style={{ marginTop: '20px', display: 'flex' }}>
+                      {lang.landing.paper3text}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+
+        <Container maxWidth='lg'>
+          <Grid container>
+            <Grid item xs={12} sm={6}>
+              <Card className={classes.cardInfo}>
+                <CardActionArea>
+                  <Typography
+                    style={{
+                      position: 'absolute',
+                      top: '15px',
+                      left: '20px',
+                      color: '#ffffff',
+                      fontWeight: 500,
+                      fontSize: '14px'
+                    }}>
+                    {lang.landing.grid2card1title}
+                  </Typography>
+                  <CardMedia
+                    image={tomasso}
+                    title='Desc Blog Picture'
+                    className={classes.tomassoPic}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant='subtitle1'>
+                      {lang.landing.grid2card1subtitle}
+                    </Typography>
+                    <Typography variant='subtitle1' color='primary'>
+                      https://www.940101.me
+                    </Typography>
+                    <Typography>{lang.landing.grid2card1text}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size='small' color='primary'>
+                    {lang.landing.grid2card1button}
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Card className={classes.cardInfo}>
+                <CardActionArea>
+                  <Typography
+                    style={{
+                      position: 'absolute',
+                      top: '15px',
+                      left: '20px',
+                      color: '#ffffff',
+                      fontWeight: 500,
+                      fontSize: '14px'
+                    }}>
+                    {lang.landing.grid2card2title}
+                  </Typography>
+                  <CardMedia
+                    image={milada}
+                    title='Desc Blog Picture'
+                    className={classes.tomassoPic}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant='subtitle1'>
+                      {lang.landing.grid2card2subtitle}
+                    </Typography>
+                    <Typography variant='subtitle1'>
+                      {lang.landing.grid2card2text}
+                      <Typography variant='body1' color='primary'>
+                        https://www.github.com/ryanwooj/devrwooWeb
+                      </Typography>
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size='small' color='primary'>
+                    {lang.landing.grid2card2button}
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </>
+    )
   );
 };
 
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  media: {
+    height: '90vh',
+    margin: 0,
+    border: 0,
+    padding: 0,
+    maxHeight: 600,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    display: 'block',
+    verticalAlign: 'top'
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  cardInfo: {
+    marginTop: theme.spacing(5),
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    marginBottom: theme.spacing(5),
+    minHeight: 150
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1)
+  },
+  avatarrr: {
+    margin: 10,
+    width: 90,
+    height: 90
+  },
+  moveRight: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(3)
+  },
+  grid2: {
+    margin: theme.spacing(2)
+  },
+  iconColor: {
+    color: '#008000	',
+    fontSize: '2rem'
+  },
+  tomassoPic: {
+    height: 200,
+    display: 'block',
+    verticalAlign: 'top'
+  }
+}));
+
 Landing.propTypes = {
-  isAuthenticated: PropTypes.bool
+  isAuthenticated: PropTypes.bool,
+  lang: PropTypes.object
 };
 
 const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
+  isAuthenticated: state.auth.isAuthenticated,
+  lang: state.language
 });
 
 export default connect(mapStateToProps)(Landing);
