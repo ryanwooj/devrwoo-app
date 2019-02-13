01e73d1f5bf4aa694ab40956c7dae8f6ac8ffc1c
Ryan Woo <devrwoo@gmail.com>
2019-02-12 18:03:09 -0800

    commentForm

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -3,13 +3,20 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addComment } from '../../actions/post';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
 const CommentForm = ({ postId, addComment }) => {
   const [text, setText] = useState('');
+
+  const classes = useStyles();
   return (
-    <div className='post-form'>
-      <div className='bg-primary p'>
-        <h3>Leave a Comment</h3>
-      </div>
+    <Container maxWidth='lg' className={classes.form}>
+      <Typography variant='h5'>Leave a Comment</Typography>
       <form
         className='form my-1'
         onSubmit={e => {
@@ -17,20 +24,39 @@ const CommentForm = ({ postId, addComment }) => {
           addComment(postId, { text });
           setText('');
         }}>
-        <textarea
-          name='text'
-          cols='30'
-          rows='5'
-          placeholder='Start Commenting!'
-          value={text}
-          onChange={e => setText(e.target.value)}
-          required
-        />
-        <input type='submit' className='btn btn-dark my-1' value='Submit' />
+        <Grid container direction='column' alignItems='flex-start'>
+          <TextField
+            label='Leave Comment'
+            multiline
+            name='text'
+            rows='5'
+            value={text}
+            margin='normal'
+            variant='outlined'
+            onChange={e => setText(e.target.value)}
+            fullWidth
+            required
+          />
+          <Grid container justify='flex-end'>
+            <Button
+              type='submit'
+              variant='contained'
+              color='primary'
+              className='btn btn-dark my-1'>
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
       </form>
-    </div>
+    </Container>
   );
 };
+
+const useStyles = makeStyles(theme => ({
+  form: {
+    marginTop: theme.spacing(3)
+  }
+}));
 
 CommentForm.propTypes = {
   addComment: PropTypes.func.isRequired
