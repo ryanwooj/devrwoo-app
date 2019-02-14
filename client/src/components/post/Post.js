4c6cefa7f2e2a93fe935b38cc8259bdef7fe777b
Ryan Woo <devrwoo@gmail.com>
2019-02-13 18:03:25 -0800

    Posts.js added

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -7,8 +7,14 @@ import PostItem from '../posts/PostItem';
 import CommentForm from './CommentForm';
 import CommentItem from './CommentItem';
 import { getPost } from '../../actions/post';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
+  const classes = useStyles();
+
   useEffect(() => {
     getPost(match.params.id);
   }, [getPost, match]);
@@ -16,21 +22,51 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
   return loading || post === null ? (
     <Spinner />
   ) : (
-    <>
-      <Link to='/posts' className='btn'>
-        {' '}
+    <Paper className={classes.paper}>
+      <Button
+        component={Link}
+        to='/posts'
+        variant='contained'
+        color='primary'
+        className={classes.button}>
         Back to Posts
-      </Link>
-      <PostItem post={post} showAction={false} />
-      <CommentForm postId={post._id} />
-      <div className='comments'>
-        {post.comments.map(comment => (
-          <CommentItem key={comment._id} comment={comment} postId={post._id} />
-        ))}
+      </Button>
+      <div className={classes.postItem}>
+        <PostItem post={post} showAction={false} />
       </div>
-    </>
+      =
+      <CommentForm postId={post._id} />
+      <Container className='comments'>
+        {post.comments.map(comment => (
+          <CommentItem
+            key={comment._id}
+            comment={comment}
+            postId={Number(post._id)}
+          />
+        ))}
+      </Container>
+    </Paper>
   );
 };
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    margin: theme.spacing(3),
+    marginTop: theme.spacing(16),
+    marginBottom: theme.spacing(10),
+    padding: theme.spacing(1),
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(7)
+  },
+  button: {
+    marginTop: theme.spacing(2),
+    marginLeft: theme.spacing(2)
+  },
+  postItem: {
+    marginTop: theme.spacing(7),
+    marginBottom: theme.spacing(1)
+  }
+}));
 
 Post.propTypes = {
   getPost: PropTypes.func.isRequired,
