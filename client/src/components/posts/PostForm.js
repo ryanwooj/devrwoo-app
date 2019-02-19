d341fb721068a9a5ac3fa7bad30cf3e57fc6eeb8
Ryan Woo <devrwoo@gmail.com>
2019-02-18 18:04:20 -0800

    Material ui integration complete

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -2,36 +2,53 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addPost } from '../../actions/post';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const PostForm = ({ addPost }) => {
   const [text, setText] = useState('');
+  const classes = useStyles();
   return (
-    <div className='post-form'>
-      <div className='bg-primary p'>
-        <h3>Say Something...</h3>
-      </div>
-      <form
-        className='form my-1'
-        onSubmit={e => {
-          e.preventDefault();
-          addPost({ text });
-          setText('');
-        }}>
-        <textarea
+    <form
+      className={classes.form}
+      onSubmit={e => {
+        e.preventDefault();
+        addPost({ text });
+        setText('');
+      }}>
+      <Grid container direction='column' alignItems='flex-start'>
+        <TextField
+          label='Write Post'
+          multiline
           name='text'
-          cols='30'
           rows='5'
-          placeholder='Create a post'
           value={text}
+          margin='normal'
+          variant='outlined'
           onChange={e => setText(e.target.value)}
+          fullWidth
           required
         />
-        <input type='submit' className='btn btn-dark my-1' value='Submit' />
-      </form>
-    </div>
+        <Grid container justify='flex-end'>
+          <Button
+            type='submit'
+            variant='contained'
+            color='primary'
+            className='btn btn-dark my-1'>
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
   );
 };
-
+const useStyles = makeStyles(theme => ({
+  form: {
+    width: '100%'
+  }
+}));
 PostForm.propTypes = {
   addPost: PropTypes.func.isRequired
 };
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -5,6 +5,13 @@ import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import { addLike, removeLike, deletePost } from '../../actions/post';
 import { getPosts } from '../../actions/post';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+import Avatar from '@material-ui/core/Avatar';
+import Badge from '@material-ui/core/Badge';
 
 const PostItem = ({
   auth,
@@ -15,55 +22,96 @@ const PostItem = ({
   post: { _id, text, name, avatar, user, likes, comments, date },
   showAction
 }) => {
+  const classes = useStyles();
   return (
-    <div className='post bg-white p-1 my-1'>
-      <div>
-        <Link to={`/profile/${user}`}>
-          <img className='round-img' src={avatar} alt='' />
-          <h4>{name}</h4>
-        </Link>
-      </div>
-      <div>
-        <p className='my-1'>{text}</p>
-        <p className='post-date'>
-          Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
-        </p>
+    <Grid
+      container
+      direction='row'
+      alignItems='flex-start'
+      className={classes.bigToSmall}>
+      <Grid item xs={12} sm={3}>
+        <Grid
+          container
+          direction='column'
+          align='center'
+          className={classes.avatarCont}>
+          <Link to={`/profile/${user}`}>
+            <Avatar className={classes.avatar} src={avatar} alt='user Avatar' />
+          </Link>{' '}
+          <Button to={`/profile/${user}`} component={Link} color='primary'>
+            {name}
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} sm={9}>
+        <Grid item className={classes.userTextField}>
+          <Typography variant='body1'>{text}</Typography>
+          <Typography variant='body2'>
+            Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
+          </Typography>
+        </Grid>
 
-        {showAction && (
-          <>
-            <button
-              onClick={e => addLike(_id)}
-              type='button'
-              className='btn btn-light'>
-              <i className='fas fa-thumbs-up' />
-              {likes.length > 0 && <span>{likes.length}</span>}
-            </button>
-            <button
-              onClick={e => removeLike(_id)}
-              type='button'
-              className='btn btn-light'>
-              <i className='fas fa-thumbs-down' />
-            </button>
-            <Link to={`/post/${_id}`} className='btn btn-primary'>
-              Discussion{' '}
-              {comments.length > 0 && (
-                <span className='comment-count'>{comments.length}</span>
-              )}
-            </Link>
-            {!auth.loading && user === auth.user._id && (
-              <button
-                onClick={e => deletePost(_id)}
-                type='button'
-                className='btn btn-danger'>
-                <i className='fas fa-times' />
-              </button>
-            )}{' '}
-          </>
-        )}
-      </div>
-    </div>
+        <Grid item className={classes.userTextField}>
+          {showAction && (
+            <Grid
+              container
+              direction='row'
+              justify='flex-start'
+              alignItems='flex-end'>
+              <Badge
+                color='primary'
+                badgeContent={likes.length > 0 ? likes.length : 0}>
+                <Button onClick={e => addLike(_id)}>
+                  <Icon>thumb_up</Icon>
+                </Button>
+              </Badge>
+              <Button onClick={e => removeLike(_id)}>
+                <Icon>thumb_down</Icon>
+              </Button>
+              <Badge
+                color='primary'
+                badgeContent={comments.length > 0 ? comments.length : 0}>
+                <Button to={`/post/${_id}`} component={Link} variant='outlined'>
+                  Comment
+                </Button>
+              </Badge>
+              {!auth.loading && user === auth.user._id && (
+                <Button
+                  onClick={e => deletePost(_id)}
+                  style={{ marginLeft: '5px' }}
+                  type='button'
+                  variant='contained'
+                  color='secondary'>
+                  <Icon>delete_forever</Icon>
+                </Button>
+              )}{' '}
+            </Grid>
+          )}
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
+
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    margin: 10,
+    width: 100,
+    height: 100
+  },
+  userTextField: {
+    margin: theme.spacing(4),
+    marginLeft: theme.spacing(3)
+  },
+  avatarCont: {
+    marginLeft: theme.spacing(3),
+    marginTop: theme.spacing(2)
+  },
+  controlCont: {
+    marginTop: theme.spacing(2)
+  },
+  bigToSmall: {}
+}));
 
 PostItem.defaultProps = {
   showAction: true
