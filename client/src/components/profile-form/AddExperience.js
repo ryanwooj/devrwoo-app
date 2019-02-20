858d2746ea897696d0a2fb4ba0762ec9cbc7e0bf
Ryan Woo <devrwoo@gmail.com>
2019-02-19 18:04:43 -0800

    Material ui integration complete

--- client/src/components/profile-form/AddExperience.js
+++ client/src/components/profile-form/AddExperience.js
@@ -1,8 +1,16 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addExperience } from '../../actions/profile';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@material-ui/core/Container';
 
 const AddExperience = ({ addExperience, history }) => {
   const [formData, setFormData] = useState({
@@ -17,107 +25,132 @@ const AddExperience = ({ addExperience, history }) => {
   const [toDateDisabled, toggleDisabled] = useState(false);
   const { company, title, location, from, to, current, description } = formData;
 
+  const classes = useStyles();
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
   return (
-    <Fragment>
-      <h1 class='large text-primary'>Add An Experience</h1>
-      <p class='lead'>
-        <i class='fas fa-code-branch' /> Add any developer/programming positions
-        that you have had in the past
-      </p>
-      <small>* = required field</small>
-      <form
-        class='form'
-        onSubmit={e => {
-          e.preventDefault();
-          addExperience(formData, history);
-        }}>
-        <div class='form-group'>
-          <input
-            type='text'
-            placeholder='* Job Title'
+    <Container justify='center' direction='column' align='center'>
+      <Paper className={classes.paper}>
+        <Typography variant='h3'>Add An Experience</Typography>
+        <Typography>Add any experience you have had in the past</Typography>
+
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            addExperience(formData, history);
+          }}>
+          <Typography variant='body2'>* = required field</Typography>
+          <TextField
+            margin='normal'
+            variant='outlined'
+            placeholder=' * Job Title'
             name='title'
             value={title}
             onChange={e => onChange(e)}
-            required
+            fullWidth
           />
-        </div>
-        <div class='form-group'>
-          <input
-            type='text'
-            placeholder='* Company'
+          <TextField
+            margin='normal'
+            variant='outlined'
+            placeholder=' * Company'
             name='company'
             value={company}
             onChange={e => onChange(e)}
-            required
+            fullWidth
           />
-        </div>
-        <div class='form-group'>
-          <input
-            type='text'
+          <TextField
+            margin='normal'
+            variant='outlined'
             placeholder='Location'
             name='location'
             value={location}
             onChange={e => onChange(e)}
+            fullWidth
           />
-        </div>
-        <div class='form-group'>
-          <h4>From Date</h4>
-          <input
-            type='date'
-            name='from'
-            value={from}
-            onChange={e => onChange(e)}
-          />
-        </div>
-        <div class='form-group'>
-          <p>
-            <input
-              type='checkbox'
-              name='current'
-              checked={current}
-              value={current}
-              onChange={e => {
-                setFormData({ ...formData, current: !current });
-                toggleDisabled(!toDateDisabled);
-              }}
-            />{' '}
-            Current Job
-          </p>
-        </div>
-        <div class='form-group'>
-          <h4>To Date</h4>
-          <input
-            type='date'
-            name='to'
-            value={to}
-            onChange={e => onChange(e)}
-            disabled={toDateDisabled ? 'disabled' : ''}
-          />
-        </div>
-        <div class='form-group'>
-          <textarea
+          <Grid container justify='space-evenly'>
+            <Grid item>
+              <TextField
+                type='date'
+                label='From'
+                name='from'
+                margin='normal'
+                value={from}
+                InputLabelProps={{
+                  shrink: true
+                }}
+                onChange={e => onChange(e)}
+              />
+              <Grid container justify='flex-start' className={classes.getTopMa}>
+                <Checkbox
+                  checked={current}
+                  name='current'
+                  value={current}
+                  onChange={e => {
+                    setFormData({ ...formData, current: !current });
+                    toggleDisabled(!toDateDisabled);
+                  }}
+                />
+                <Typography style={{ marginTop: '.5em' }}>Current</Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <TextField
+                type='date'
+                label='To'
+                name='to'
+                value={to}
+                margin='normal'
+                InputLabelProps={{
+                  shrink: true
+                }}
+                onChange={e => onChange(e)}
+                disabled={toDateDisabled ? true : false}
+              />
+            </Grid>
+          </Grid>
+          <TextField
+            margin='normal'
             name='description'
-            cols='30'
-            rows='5'
-            placeholder='Job Description'
             value={description}
+            placeholder='Job Description'
+            fullWidth
             onChange={e => onChange(e)}
           />
-        </div>
-        <input type='submit' class='btn btn-primary my-1' />
-        <Link class='btn btn-light my-1' to='/dashboard'>
-          Go Back
-        </Link>
-      </form>
-    </Fragment>
+          <Button
+            margin='normal'
+            to='/dashboard'
+            component={Link}
+            variant='outlined'
+            color='primary'>
+            Go Back
+          </Button>{' '}
+          <Button
+            margin='normal'
+            type='submit'
+            variant='outlined'
+            color='primary'>
+            Submit
+          </Button>
+        </form>
+      </Paper>
+    </Container>
   );
 };
 
+const useStyles = makeStyles(theme => ({
+  paper: {
+    margin: theme.spacing(2),
+    padding: theme.spacing(4)
+  },
+  gridmargin: {
+    topMargin: theme.spacing(3)
+  }
+}));
+
 AddExperience.propTypes = {
-  AddExperience: PropTypes.func.isRequired
+  AddExperience: PropTypes.func,
+  history: PropTypes.object.isRequired
 };
 
 export default connect(
