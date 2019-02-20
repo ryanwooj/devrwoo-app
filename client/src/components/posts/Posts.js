77bf6a91d48b3ab00388fed499d5224fd1068306
Ryan Woo <devrwoo@gmail.com>
2019-02-19 18:04:33 -0800

    Material ui integration complete

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -5,30 +5,48 @@ import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
 import PostForm from './PostForm';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
 const Posts = ({ getPosts, posts: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts, loading]);
+  const classes = useStyles();
   return loading ? (
     <Spinner />
   ) : (
-    <>
-      <h1 className='large text-primary'>Posts</h1>
-      <p className='lead'>
-        <i className='fas fa-user' />
-        Welcome to the Community
-      </p>
-      {/* {PostForm} */}
-      <PostForm />
-      <div className='posts'>
-        {posts.map(post => (
-          <PostItem key={post._id} post={post} />
-        ))}
-      </div>
-    </>
+    <Container maxWidth='lg'>
+      <Paper className={classes.paper}>
+        <Grid
+          container
+          direction='column'
+          justify='flex-start'
+          alignItems='flex-start'>
+          <Typography variant='h1'>Posts</Typography>
+          <PostForm />
+          <Grid item>
+            {posts.map(post => (
+              <PostItem key={post._id} post={post} />
+            ))}
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
   );
 };
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(16),
+    padding: theme.spacing(3),
+    paddingTop: theme.spacing(5),
+    paddingBottom: theme.spacing(10),
+    marginBottom: theme.spacing(16)
+  }
+}));
 
 Posts.propTypes = {
   getPosts: PropTypes.func.isRequired,
