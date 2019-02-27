d62bff1c25a7dc800ca4b99fbc76fcfa2a9cbee0
Ryan Woo <devrwoo@gmail.com>
2019-02-26 18:07:04 -0800

    Material ui for Profile

--- client/src/components/profiles/Profileitem.js
+++ client/src/components/profiles/Profileitem.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import Icon from '@material-ui/core/Icon';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 const Profileitem = ({
   profile: {
@@ -9,32 +18,70 @@ const Profileitem = ({
     company,
     location,
     skills
-  }
+  },
+  lang
 }) => {
+  const classes = useStyles();
   return (
-    <div className='profile bg-light'>
-      <img src={avatar} alt='' className='round-img' />
-      <div>
-        <h2>{name}</h2>
-        <p>
-          {status} {company && <span> at {company}</span>}{' '}
-        </p>
-        <p className='my-1'>{location && <span>{location}</span>}</p>
-        <Link to={`/profile/${_id}`} className='btn btn-primary'>
-          View Profile
-        </Link>
-      </div>
-      <ul>
-        {skills.slice(0, 4).map((skill, index) => (
-          <li key={index} className='text-primary'>
-            <i className='fas fa-check'> </i>
-            {skill}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Grid item xs={12} sm={6} md={4} className={classes.topmama}>
+      <Card className={classes.card}>
+        <Grid container direction='column' justify='center' alignItems='center'>
+          <CardMedia
+            src={avatar}
+            alt='user Avatar'
+            className={classes.media}
+            component='img'
+          />
+          <CardContent>
+            <Typography variant='h5'>{name}</Typography>
+            <Typography variant='body1' color='textSecondary'>
+              {status} {company && <span> at {company}</span>}{' '}
+            </Typography>
+            <Typography variant='body1' color='textSecondary'>
+              {location && <span>{location}</span>}
+            </Typography>
+          </CardContent>
+          <Grid container align='center'>
+            {skills.slice(0, 4).map((skill, index) => (
+              <Grid item xs={6} key={index}>
+                <Icon>check</Icon>
+                {skill}
+              </Grid>
+            ))}
+          </Grid>
+          <CardActions>
+            <Button
+              to={`/profile/${_id}`}
+              component={Link}
+              color='primary'
+              className={classes.button}>
+              {lang.profiles.button}
+            </Button>
+          </CardActions>
+        </Grid>
+      </Card>
+    </Grid>
   );
 };
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  topmama: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2)
+  },
+  card: {
+    margin: theme.spacing(1)
+  },
+  media: {
+    height: 175
+  },
+  button: {
+    right: 1
+  }
+}));
 
 Profileitem.propTypes = {
   profile: PropTypes.object.isRequired
