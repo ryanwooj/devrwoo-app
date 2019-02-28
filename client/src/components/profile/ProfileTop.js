ab9006b303aa80ebe4effe7db672f18ee2040e66
Ryan Woo <devrwoo@gmail.com>
2019-02-27 18:07:17 -0800

    Material ui for Profile

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import Paper from '@material-ui/core/Paper';
+import Icon from '@material-ui/core/Icon';
 
 const ProfileTop = ({
   profile: {
@@ -11,49 +18,80 @@ const ProfileTop = ({
     user: { name, avatar }
   }
 }) => {
+  const classes = useStyles();
   return (
-    <div className='profile-top bg-primary p-2'>
-      <img className='round-img my-1' src={avatar} alt='' />
-      <h1 className='large'>{name}</h1>
-      <p className='lead'>
-        {status} {company && <span> at {company}</span>}
-      </p>
-      <p>{location && <span>{location}</span>}</p>
-      <div className='icons my-1'>
-        {website && (
-          <a href={website} target='_blank' rel='noopener noreferrer'>
-            <i className='fas fa-globe fa-2x' />
-          </a>
-        )}
-        {social && social.twitter && (
-          <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-twitter fa-2x' />
-          </a>
-        )}
-        {social && social.facebook && (
-          <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-facebook fa-2x' />
-          </a>
-        )}
-        {social && social.linkedin && (
-          <a href={social.linkedin} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-linkedin fa-2x' />
-          </a>
-        )}
-        {social && social.youtube && (
-          <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-youtube fa-2x' />
-          </a>
-        )}
-        {social && social.instagram && (
-          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-instagram fa-2x' />
-          </a>
-        )}
-      </div>
-    </div>
+    <Paper className={classes.paper}>
+      <Grid container direction='row' justify='space-evenly'>
+        <Grid item>
+          <Avatar className={classes.media} src={avatar} alt='' />
+        </Grid>
+        <Grid item t={3}>
+          <Typography variant='h5'>{name}</Typography>
+          <Typography className='lead'>
+            {status} {company && <span> at {company}</span>}
+          </Typography>
+          <Typography>{location && <span>{location}</span>}</Typography>
+          <Grid container direction='row'>
+            <Grid item>
+              {website && (
+                <Button href={website} color='secondary'>
+                  <Icon fontSize='large'>directions</Icon> to Website
+                </Button>
+              )}
+            </Grid>
+            <Grid item>
+              {social && social.twitter && (
+                <Button href={social.twitter}>
+                  <Avatar src={require('../../images/twitter.svg')} />
+                </Button>
+              )}
+            </Grid>
+            <Grid item>
+              {social && social.facebook && (
+                <Button href={social.facebook}>
+                  <Avatar src={require('../../images/facebook.svg')} />
+                </Button>
+              )}
+            </Grid>
+            <Grid item>
+              {social && social.linkedin && (
+                <Button href={social.linkedin}>
+                  <Avatar src={require('../../images/linkedin.svg')} />
+                </Button>
+              )}
+            </Grid>
+            <Grid item>
+              {social && social.youtube && (
+                <Button href={social.youtube}>
+                  <Avatar src={require('../../images/youtube.svg')} />
+                </Button>
+              )}
+            </Grid>
+            <Grid item>
+              {social && social.instagram && (
+                <Button href={social.instagram}>
+                  <Avatar src={require('../../images/instagram.svg')} />
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
+const useStyles = makeStyles(theme => ({
+  media: {
+    margin: 10,
+    width: 100,
+    height: 100
+  },
+  paper: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+    minHeight: 210
+  }
+}));
 
 ProfileTop.propTypes = {
   profile: PropTypes.object.isRequired
