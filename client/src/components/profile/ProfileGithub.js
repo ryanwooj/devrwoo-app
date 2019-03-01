ea81b0d03e4558820fa73d78c18f94fe6c68ce84
Ryan Woo <devrwoo@gmail.com>
2019-02-28 18:07:38 -0800

    Material ui for Profile

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -4,62 +4,102 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getGithubRepos } from '../../actions/profile';
 import Moment from 'react-moment';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
 
 const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   useEffect(() => {
     getGithubRepos(username);
   }, [getGithubRepos, username]);
 
+  const classes = useStyles();
   return (
-    <div className='profile-github'>
-      <h2 className='text-primary my-1'>Github Repos</h2>
-      {repos === null ? (
-        <Spinner />
+    <>
+      {repos.length > 0 ? (
+        <Grid
+          container
+          direction='row'
+          justify='space-evenly'
+          alignItems='flex-start'>
+          <Grid item xs={12}>
+            <Typography variant='h4'>Github Repos</Typography>
+          </Grid>
+          {repos === null ? (
+            <Spinner />
+          ) : (
+            repos.map(repo => {
+              return (
+                <Grid key={repo.id} item xs={12} sm={6} md={4}>
+                  <Grid>
+                    <Typography variant='h4' color='primary'>
+                      <Button href={repo.html_url} color='primary'>
+                        {repo.name}
+                      </Button>
+                    </Typography>
+                    {repo.language && (
+                      <Typography> Language: {repo.language} </Typography>
+                    )}
+                    <Button
+                      href={repo.html_url}
+                      color='secondary'
+                      className={classes.noCaps}>
+                      https://www.github.com/{repo.full_name}
+                    </Button>
+                    {repo.created_at && (
+                      <Typography>
+                        Created at{' '}
+                        <Moment format='MM/DD/YYYY'>{repo.created_at}</Moment>{' '}
+                      </Typography>
+                    )}
+                  </Grid>
+                  <Grid container direction='row' justify='space-evenly'>
+                    <Grid item>
+                      <Icon className={classes.starIcon}>star</Icon>
+                      <Typography> Stars: {repo.stargazers_count}</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Icon className={classes.eyeIcon}>remove_red_eye</Icon>
+                      <Typography>Watchers: {repo.watchers_count}</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Icon className={classes.setIcon}>
+                        settings_input_component
+                      </Icon>
+                      <Typography>Forks: {repo.forks_count}</Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              );
+            })
+          )}
+        </Grid>
       ) : (
-        repos.map(repo => {
-          // console.log(repo);
-          return (
-            <div key={repo.id} className='repo bg-white p-1 my-1'>
-              <div>
-                <h4>
-                  <a
-                    href={repo.html_url}
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    {repo.name}
-                  </a>
-                </h4>
-                {repo.language && <p> Language: {repo.language} </p>}
-                <a href={repo.html_url}>
-                  https://www.github.com/{repo.full_name}
-                </a>
-                {repo.created_at && (
-                  <p>
-                    Created at{' '}
-                    <Moment format='MM/DD/YYYY'>{repo.created_at}</Moment>{' '}
-                  </p>
-                )}
-              </div>
-              <div>
-                <ul>
-                  <li className='badge badge-primary'>
-                    Stars: {repo.stargazers_count}
-                  </li>
-                  <li className='badge badge-dark'>
-                    Watchers: {repo.watchers_count}
-                  </li>
-                  <li className='badge badge-light'>
-                    Forks: {repo.forks_count}
-                  </li>
-                </ul>
-              </div>
-            </div>
-          );
-        })
+        <Typography variant='h5'>Empty Repository</Typography>
       )}
-    </div>
+    </>
   );
 };
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: theme.spacing(2)
+  },
+  noCaps: {
+    textTransform: 'lowercase'
+  },
+  starIcon: {
+    color: '#ffea00'
+  },
+  eyeIcon: {
+    color: '#673ab7'
+  },
+  setIcon: {
+    color: 'black'
+  }
+}));
 
 ProfileGithub.propTypes = {
   getGithubRepos: PropTypes.func.isRequired,
