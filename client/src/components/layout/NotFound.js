cd0c566fa5a9b97fe3f2afbc699230f2f6164aa3
Ryan Woo <devrwoo@gmail.com>
2019-03-23 18:26:43 -0700

    Material Ui Integration

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 const NotFound = () => {
   return (
     <>
+      <Container maxWindth='lg'>
+        <Grid container direction='column' justify='center' alignItems='center'>
+          <Grid item>
+            <Typography variant='h1'>PAGE NOT FOUND</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant='h2' color='secondary'>
+              404 ERROR...{' '}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant='h3'>
+              This page does not exist. Don't Come back!
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
       <h1 className='x-large text-primary'>
         <i className='fas fa-exclamation-triangle' />
         Page Not Found
