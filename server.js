f06331cc3bd8789969fae94acea3644aad4be15c
Ryan Woo <devrwoo@gmail.com>
2019-06-12 17:42:52 -0700

    Initialized Server

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const compression = require('compression');
 const path = require('path');
-
+const bodyParser = require('body-parser');
+const config = require('config');
 const app = express();
+
+//Compress The files to optimize the setting
+app.use(compression());
 
 //Connect Database
 connectDB();
 
-//Init Middleware
-app.use(express.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -25,6 +30,21 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+//Enforcing https
+app.all('*', function(req, res, next) {
+  console.log(
+    'req start: ',
+    req.secure,
+    req.hostname,
+    req.url,
+    app.get('port')
+  );
+  if (req.secure) {
+    return next();
+  }
+  res.redirect('https://' + req.hosthame + ':' + app.get('secPort') + req.url);
+});
 
 //Define Port & use 4000 if doesn't have one
 const PORT = process.env.PORT || 4000;
